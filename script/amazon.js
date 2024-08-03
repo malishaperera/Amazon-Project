a8df2beea95156f06a0d6d9f1964a499acddabdd
Malisha Perera <140098754+malishaperera@users.noreply.github.com>
2024-08-03 10:44:20 +0530

    calculate addtoCart quantity

--- script/amazon.js
+++ script/amazon.js
@@ -127,11 +127,23 @@ document.querySelectorAll('.js-add-to-cart')
         productId:productId,
         quantity: 1
       });
-
     }
-    console.log(cart);
-    
 
+    //calculate quantity
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+
+    });
+
+    document.querySelector('.js-cart-quantity')
+    .innerHTML = cartQuantity;
+    
+    console.log(cartQuantity);
+
+
+
+    console.log(cart);
   });
 
 });
