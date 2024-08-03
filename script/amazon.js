ad872f23414281b09d876a55e157e00f305f6269
Malisha Perera <140098754+malishaperera@users.noreply.github.com>
2024-08-03 11:50:44 +0530

    use modules and import cart form cart.js

--- script/amazon.js
+++ script/amazon.js
@@ -1,44 +1,6 @@
-// const products = [{
-//   img: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-//   name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
-//   rating:{
-//     stars:4.5,
-//     count:87
-//   },
-//   priceCents:1090
-// },
-
-// {
-//   img: 'images/products/intermediate-composite-basketball.jpg',
-//   name:'Intermediate Size Basketball',
-//   rating:{
-//     stars:4,
-//     count:127
-//   },
-//   priceCents:2095
-// },
-
-
-// {
-//   img: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-//   name:'Adults Plain Cotton T-Shirt - 2 Pack',
-//   rating:{
-//     stars:4.5,
-//     count:56
-//   },
-//   priceCents:799
-// },
-
-// {
-//   img: 'images/products/black-2-slot-toaster.jpg',
-//   name: '2 Slot Toaster - Black',
-//   rating:{
-//     stars:5,
-//     count:2197
-//   },
-//   priceCents:1899
-
-// }];
+import{cart} from '../data/cart.js';
+//import{cart as myCart} from '../data/cart.js'; <<< this name change(rename)
+// const cart = [];
 
 let productsHTML = '';
 
@@ -138,7 +100,7 @@ document.querySelectorAll('.js-add-to-cart')
 
     document.querySelector('.js-cart-quantity')
     .innerHTML = cartQuantity;
-    
+
     console.log(cartQuantity);
 
 
