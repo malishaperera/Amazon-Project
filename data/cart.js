b137b8c96d9e79bc1ddedf694a9ae13492ab4ed4
Malisha Perera <140098754+malishaperera@users.noreply.github.com>
2024-10-23 08:41:38 +0530

    Move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,24 @@
 export const cart = [];
 
+export function addToCart(productId){
+  let matchingItem;
+
+  cart.forEach((item) => {
+    if(productId === item.productId){
+      matchingItem = item;
+    }
+  });
+
+  if(matchingItem){
+    matchingItem.quantity += 1;
+  }else{
+    cart.push({
+      productId:productId,
+      quantity:1
+    });
+  }
+}
+
 
 
 
